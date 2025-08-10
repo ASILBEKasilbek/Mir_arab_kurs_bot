d4e720d8aa05eed1984d9c20c6aab1d5c212d9a7
Asilbek <asilbek.sadullayev000@example.com>
2025-08-10 18:15:20 +0500

    seconr commit

--- main.py
+++ main.py
@@ -4,7 +4,13 @@ from aiogram.fsm.storage.memory import MemoryStorage
 from config import BOT_TOKEN
 from database import init_db
 from handlers.registration import register_handlers
+from aiogram import types
 
+async def set_default_commands(bot: Bot):
+    commands = [
+        types.BotCommand(command="start", description="⚪️ Botni ishga tushirish"),
+    ]
+    await bot.set_my_commands(commands)
 
 async def main():
     init_db()
@@ -12,6 +18,7 @@ async def main():
     dp = Dispatcher(storage=MemoryStorage())
 
     register_handlers(dp)
+    await set_default_commands(bot)
 
     print("Bot ishga tushmoqda...")
     await dp.start_polling(bot)
